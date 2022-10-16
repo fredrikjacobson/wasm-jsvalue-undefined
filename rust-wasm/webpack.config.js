bdb950e5b35cf2607c83b3b1576a04d0d0f0c030
fredrik jacobson <fredrik.jacobson@annotell.com>
2022-10-16 20:50:47 +0200

    Repro of issue

---
+++ rust-wasm/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+		new HtmlWebpackPlugin({ template: './index.html' }),
+  ],
+	experiments: {
+		asyncWebAssembly: true
+	}
+};
